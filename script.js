80ee14adb72f5525e3f9f2e47a824626cdfca592
Austin Moninger <austinmoninger@gmail.com>
2020-07-20 22:44:15 -0500

    Timer mvp

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-document.getElementsByClassName('r-2llsf')[0].style.display = 'none'
 
+function eradicate() {
+    function eradicateRetry() {
+        const div = document.querySelector(`[aria-label="Timeline: Your Home Timeline"]`)
+
+        if (div == null) {
+            return
+        } else {
+            div.style.display = 'none'
+        }
+    }
+    setInterval(eradicateRetry, 1000)
+}
+
+const start = 8 * 60 + 30;
+const end = 17 * 60 + 0;
+
+function inTime() {
+    var now = new Date();
+    var time = now.getHours() * 60 + now.getMinutes();
+    // return time >= start && time < end;
+    return true
+}
+
+if (inTime()) {
+    eradicate()
+}

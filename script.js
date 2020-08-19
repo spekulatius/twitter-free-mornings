37e818383a3a11f1a4e5c0820d7f75e5f66dd833
Austin Moninger <austinmoninger@gmail.com>
2020-08-19 14:23:10 -0700

    Hide side bar

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 
 function eradicate() {
     function eradicateRetry() {
-        const div = document.querySelector(`[aria-label="Timeline: Your Home Timeline"]`)
+        const timeline = document.querySelector(`[aria-label="Timeline: Your Home Timeline"]`)
+        // const follow = document.querySelector(`[aria-label="Who to follow"]`).parentNode
+        // const trending = document.querySelector(`[aria-label="Timeline: Trending now"]`)
+        const sidebar = document.querySelector(`[data-testid="sidebarColumn"]`)
+        
 
-        if (div == null) {
+        if (sidebar == null) {
             return
         } else {
-            div.style.display = 'none'
+            timeline.style.display = 'none'
+            // follow.style.display = 'none'
+            sidebar.style.display = 'none'
         }
     }
     setInterval(eradicateRetry, 1000)
 }
 
-const start = 8 * 60 + 30;
+const start = 8 * 60 + 0;
 const end = 17 * 60 + 0;
 
 function inTime() {
-    var now = new Date();
-    var time = now.getHours() * 60 + now.getMinutes();
-    // return time >= start && time < end;
-    return true
+    const now = new Date();
+    const time = now.getHours() * 60 + now.getMinutes();
+    console.log('TIME', start, time, end)
+    return time >= start && time < end;
+    // return true
 }
 
 if (inTime()) {

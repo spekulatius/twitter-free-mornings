91bfba011c82d8ebd0cd4ef79e9b22c7651bcc58
Austin Moninger <austinmoninger@gmail.com>
2020-08-31 14:45:18 -0600

    Added timer functionality

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ function isAlreadyInjected() {
 }
 
 function injectUI(timelineParent) {
+
+    prevContainer = document.querySelector('#timer-container')
+
+    if (prevContainer != null) {
+        prevContainer.remove()
+    }
+
     const container = document.createElement('div')
     container.id = 'timer-container'
     timelineParent.appendChild(container)
@@ -14,7 +21,8 @@ function injectUI(timelineParent) {
     container.appendChild(header)
 
     const timer = document.createElement('p')
-    const timerText = document.createTextNode('00:00:00')
+    const timerString = getTimerString()
+    const timerText = document.createTextNode(timerString)
     timer.appendChild(timerText)
     container.appendChild(timer)
 
@@ -43,11 +51,11 @@ function replaceTimeline() {
         const timelineParent = timeline.parentNode
 
         // Add container with timer and copy
-        if (timelineParent && !isAlreadyInjected()) {
-            injectUI(timelineParent)
-        } else if (isAlreadyInjected()) {
+        // if (timelineParent && !isAlreadyInjected()) {
+        injectUI(timelineParent)
+        // } else if (isAlreadyInjected()) {
             // TODO: update timer
-        }
+        // }
     }
     setInterval(replaceTimelineRetry, 1000)
 }
@@ -59,6 +67,7 @@ function removeSidebar() {
             return
         } else {
             sidebar.style.display = 'none'
+            // sidebar.remove()
         }
 
     }
@@ -70,11 +79,38 @@ const start = 8 * 60 + 0;
 const end = 17 * 60 + 0;
 
 function inTime() {
+    // TODO there's probably something wrong with time zones here
     const now = new Date();
     const time = now.getHours() * 60 + now.getMinutes();
     console.log('TIME', start, time, end)
-    // return time >= start && time < end;
-    return true
+    return time >= start && time < end;
+    // return true
+}
+
+function getTimerString() {
+
+    const countDownDate = new Date("Sep 5, 2020 15:37:25").getTime();
+
+    const now = new Date().getTime()
+
+    const distance = countDownDate - now
+
+
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+    return formatNumber(hours) + ':' + formatNumber(minutes) + ':' + formatNumber(seconds)
+
+
+
+}
+
+function formatNumber(num) {
+    const s = num + ''
+    if (s.length == 0) return '00'
+    if (s.length == 1) return '0' + s
+    return s
 }
 
 removeSidebar()
